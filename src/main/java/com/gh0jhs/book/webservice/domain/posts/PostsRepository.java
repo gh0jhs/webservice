cee43286654a534a5651372e982dd87232599631
gh0jhs <gh0jhs@naver.com>
2021-08-29 02:44:50 +0900

    webservice #3

---
+++ src/main/java/com/gh0jhs/book/webservice/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.gh0jhs.book.webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
