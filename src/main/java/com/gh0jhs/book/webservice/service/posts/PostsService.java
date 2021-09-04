1363fbdf6e4046b3708fd4c5a1cedff856f0e948
gh0jhs <gh0jhs@naver.com>
2021-09-04 22:46:21 +0900

    webservice #4

--- src/main/java/com/gh0jhs/book/webservice/service/posts/PostsService.java
+++ src/main/java/com/gh0jhs/book/webservice/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.gh0jhs.book.webservice.service.posts;
 
 import com.gh0jhs.book.webservice.domain.posts.Posts;
 import com.gh0jhs.book.webservice.domain.posts.PostsRepository;
+import com.gh0jhs.book.webservice.web.dto.PostsListResponseDto;
 import com.gh0jhs.book.webservice.web.dto.PostsResponseDto;
 import com.gh0jhs.book.webservice.web.dto.PostsSaveRequestDto;
 import com.gh0jhs.book.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,18 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
